cbf1b01f710a56bded7f6ef587e0cf9f20ecd6dd
variyapurv <133880947+pvariya@users.noreply.github.com>
2025-02-18 12:03:47 +0530

    Update UserSignup.jsx

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { UserdataContext } from '../context/userContext';
+import { UserdataContext } from '../context/UserContext';
 
 
 const UserSignup = () => {
