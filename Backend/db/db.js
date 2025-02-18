d3fee3a1e9ed7504a43bf6edba138d6d40e4f53e
variyapurv <133880947+pvariya@users.noreply.github.com>
2025-02-18 12:15:41 +0530

    Update db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/uber");
+    await mongoose.connect("mongodb+srv://variyapurv4211:purv@cluster0.eivrm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
     console.log("Connected to DB");
   } catch (error) {
