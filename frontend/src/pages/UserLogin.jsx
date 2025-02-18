3fee130e20c245842c99050bc7ffaf9b680cb2ff
variyapurv <133880947+pvariya@users.noreply.github.com>
2025-02-18 12:04:06 +0530

    Update UserLogin.jsx

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
-import { UserdataContext } from '../context/userContext'
+import { UserdataContext } from '../context/UserContext'
 import axios from 'axios'
 
 
